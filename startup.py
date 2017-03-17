79908d1434ccd932bb762917823b8281cd5c5291
ben rudgers <ben.rudgers@gmail.com>
2017-03-17 16:12:24 -0500

    first version of startup files

---
+++ startup.py
@@ -0,0 +1,9 @@
+import os
+from os import getcwd as pwd
+from os import chdir as cd
+from os import listdir as ls
+from os import mkdir as mkdir
+
+from sys import path as path
+path.append('/home/benrudgers/python/using')
+from using import using as using
